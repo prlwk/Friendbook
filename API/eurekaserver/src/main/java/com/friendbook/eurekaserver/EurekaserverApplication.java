6c31faa2e89369bf2fbf86b878abd0c8dfb02adb
prlwk <molchanova2.aa@edu.spbstu.ru>
2022-09-19 17:55:51 +0300

    Add filles for Docker

--- API/eurekaserver/src/main/java/com/friendbook/eurekaserver/EurekaserverApplication.java
+++ API/eurekaserver/src/main/java/com/friendbook/eurekaserver/EurekaserverApplication.java
@@ -2,10 +2,12 @@ package com.friendbook.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
--- API/user-service/src/main/java/com/friendbook/userservice/UserServiceApplication.java
+++ API/user-service/src/main/java/com/friendbook/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.friendbook.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class UserServiceApplication {
 
     public static void main(String[] args) {
