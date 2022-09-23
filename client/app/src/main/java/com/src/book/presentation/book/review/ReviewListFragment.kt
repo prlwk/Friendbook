b8a78676922f2fb696bd0eb937f4244eeb78259c
Андрей Усанов <iambreaddd@mail.ru>
2022-09-23 14:48:34 +0300

    Add book review list night mode working correctly

--- client/app/src/main/java/com/src/book/presentation/book/review/ReviewListFragment.kt
+++ client/app/src/main/java/com/src/book/presentation/book/review/ReviewListFragment.kt
@@ -1,16 +1,21 @@
 package com.src.book.presentation.book.review
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import com.src.book.R
+import com.src.book.databinding.FragmentReviewListBinding
 
 //TODO: при большом общем количестве отзывов весь текст числа отзывов не влазит
 //TODO: надо сделать чтоб пятизначные цифры из, например, "11563" превращались в "11к"
 
 class ReviewListFragment : Fragment() {
+
+    private lateinit var binding: FragmentReviewListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,4 +29,8 @@ class ReviewListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_review_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentReviewListBinding.bind(view);
+    }
 }
