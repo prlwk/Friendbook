155e6151d30f98fe846806f3faabe5de5ecdcee1
Danil-Pashkov <taverna201@gmail.com>
2022-09-25 19:41:08 +0300

    Add send request friend page(light and dark theme)

---
+++ client/app/src/main/java/com/src/book/presentation/friends/add_friends/AddFriendsFragment.kt
@@ -0,0 +1,21 @@
+package com.src.book.presentation.friends.add_friends
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.src.book.R
+
+class AddFriendsFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_friends, container, false)
+    }
+
+    companion object {
+    }
+}
