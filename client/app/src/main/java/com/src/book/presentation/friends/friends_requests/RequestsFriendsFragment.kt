bb2420a818a29e13c4cf8067287cdad691ad0984
Danil-Pashkov <taverna201@gmail.com>
2022-09-29 16:05:48 +0300

    Add requests page without black mode

---
+++ client/app/src/main/java/com/src/book/presentation/friends/friends_requests/RequestsFriendsFragment.kt
@@ -0,0 +1,24 @@
+package com.src.book.presentation.friends.friends_requests
+
+import android.content.res.Resources
+import android.graphics.Outline
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import androidx.fragment.app.Fragment
+import com.src.book.R
+
+class RequestsFriendsFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_requests_friends, container, false)
+    }
+
+    companion object {
+    }
+}
