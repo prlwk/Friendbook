9c3f66dde9a1a34b0d4fb9e9369199a9dfcc4a72
prlwk <molchanova2.aa@edu.spbstu.ru>
2022-10-23 22:50:23 +0300

    Create MailService

--- API/user-service/src/main/java/com/friendbook/userservice/controller/MailController.java
+++ API/user-service/src/main/java/com/friendbook/userservice/controller/MailController.java
@@ -1,57 +1,29 @@
 package com.friendbook.userservice.controller;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.friendbook.userservice.model.User;
-import com.friendbook.userservice.model.VerifyCode;
-import com.friendbook.userservice.service.UserService;
-import com.friendbook.userservice.service.VerifyCodeService;
+import com.friendbook.userservice.service.MailService;
 import com.friendbook.userservice.utils.AppError;
-import com.friendbook.userservice.utils.EmailText;
 
 @RestController
 public class MailController {
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
-    private VerifyCodeService verifyCodeService;
+    private MailService mailService;
 
     @RequestMapping(path = "/send-code-for-confirmation-account", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> sendConfirmationAccountCode(@RequestParam Long userId) {
+    public ResponseEntity<?> sendConfirmationAccountCode(@RequestParam Long id) {
         try {
-            User user = userService.getUserById(userId);
-            VerifyCode confirmationCode = new VerifyCode(user);
-            verifyCodeService.saveCode(confirmationCode);
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper;
-            helper = new MimeMessageHelper(message, true);
-            helper.setFrom("plum.bestcompany@gmail.com");
-            helper.setTo(user.getEmail());
-            helper.setText(EmailText.firstPartEmailText +
-                    "Подтверждение почты" +
-                    EmailText.secondPаrtEmailText +
-                    "Добрый день, " + user.getName() + "! Чтобы подтвердить свой аккаунт, нужно ввести код в приложение." +
-                    EmailText.thirdPаrtEmailText +
-                    confirmationCode.getCode() +
-                    EmailText.fourthPartEmailText, true);
-            helper.setSubject("Подтверждение аккаунта");
-            javaMailSender.send(message);
+            mailService.sendConfirmationCode(id);
         } catch (MessagingException e) {
             return new ResponseEntity<>(
                     new AppError(HttpStatus.NOT_FOUND.value(),
@@ -59,7 +31,7 @@ public class MailController {
         } catch (EntityNotFoundException exception) {
             return new ResponseEntity<>(
                     new AppError(HttpStatus.NOT_FOUND.value(),
-                            "User with id " + userId + " does not exist."), HttpStatus.NOT_FOUND);
+                            "User with id " + id + " does not exist."), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -67,22 +39,7 @@ public class MailController {
     @RequestMapping(path = "/send-code-for-recovery-password", method = RequestMethod.GET)
     public ResponseEntity<?> sendPasswordRecoveryCode(@RequestParam String email) {
         try {
-            User user = userService.findUserByEmail(email);
-            VerifyCode passwordRecoveryCode = new VerifyCode(user);
-            verifyCodeService.saveCode(passwordRecoveryCode);
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper;
-            helper = new MimeMessageHelper(message, true);
-            helper.setTo(email);
-            helper.setText(EmailText.firstPartEmailText +
-                    "Восстановление пароля" +
-                    EmailText.secondPаrtEmailText +
-                    "Добрый день, " + user.getName() + "! Чтобы восстановить пароль, пожалуйста, введите в приложение данный код" +
-                    EmailText.thirdPаrtEmailText +
-                    passwordRecoveryCode.getCode() +
-                    EmailText.fourthPartEmailText, true);
-            helper.setSubject("Восстановление пароля!");
-            javaMailSender.send(message);
+            mailService.sendPasswordRecoveryCode(email);
         } catch (MessagingException e) {
             return new ResponseEntity<>(
                     new AppError(HttpStatus.BAD_REQUEST.value(),
