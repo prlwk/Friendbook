5d4618776843d2e1aec318c615a786aaf10c522d
hella-r-02 <77581078+hella-r-02@users.noreply.github.com>
2022-10-24 21:19:02 +0300

    Add tests for SignInViewModel

---
+++ client/app/src/test/java/com/src/book/presentation/login/SignInViewModelTest.kt
@@ -0,0 +1,78 @@
+package com.src.book.presentation.login
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.src.book.EMAIL
+import com.src.book.PASSWORD
+import com.src.book.domain.usecase.SignInUseCase
+import com.src.book.domain.utils.LoginState
+import com.src.book.presentation.registration.sign_in.viewModel.SignInViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class SignInViewModelTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+    private lateinit var signInUseCase: SignInUseCase
+    private lateinit var signInViewModel: SignInViewModel
+    private val dispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(dispatcher = dispatcher)
+        signInUseCase = mockk()
+        signInViewModel = SignInViewModel(signInUseCase = signInUseCase)
+    }
+
+    @After
+    fun cleanUp() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun signInRequestSuccessful() = runTest {
+        coEvery { signInUseCase.execute(any(), any(), any()) } returns LoginState.SuccessState
+        signInViewModel.signInRequest(EMAIL, PASSWORD, true)
+        Assert.assertEquals(LoginState.SuccessState, signInViewModel.liveDataLoginState.value)
+        Assert.assertEquals(false, signInViewModel.liveDataIsLoading.value)
+    }
+
+    @Test
+    fun signInRequestError() = runTest {
+        coEvery { signInUseCase.execute(any(), any(), any()) } returns LoginState.ErrorServerState
+        signInViewModel.signInRequest(EMAIL, PASSWORD, true)
+        Assert.assertEquals(LoginState.ErrorServerState, signInViewModel.liveDataLoginState.value)
+        Assert.assertEquals(false, signInViewModel.liveDataIsLoading.value)
+    }
+
+    @Test
+    fun signInRequestErrorEmailLogin() = runTest {
+        coEvery {
+            signInUseCase.execute(any(), any(), any()) } returns LoginState.ErrorEmailLoginState
+        signInViewModel.signInRequest(EMAIL, PASSWORD, true)
+        Assert.assertEquals(
+            LoginState.ErrorEmailLoginState,
+            signInViewModel.liveDataLoginState.value
+        )
+        Assert.assertEquals(false, signInViewModel.liveDataIsLoading.value)
+    }
+
+    @Test
+    fun signInRequestErrorPassword() = runTest {
+        coEvery { signInUseCase.execute(any(), any(), any()) } returns LoginState.ErrorPasswordState
+        signInViewModel.signInRequest(EMAIL, PASSWORD, true)
+        Assert.assertEquals(LoginState.ErrorPasswordState, signInViewModel.liveDataLoginState.value)
+        Assert.assertEquals(false, signInViewModel.liveDataIsLoading.value)
+    }
+}
