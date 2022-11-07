68b47c19f2d23769d86a3f4d5aff32cb9919c813
prlwk <molchanova2.aa@edu.spbstu.ru>
2022-11-07 14:11:50 +0300

    Add request for save and delete book to bookmarks

--- API/user-service/src/main/java/com/friendbook/userservice/repository/UserBooksWantToReadRepository.java
+++ API/user-service/src/main/java/com/friendbook/userservice/repository/UserBooksWantToReadRepository.java
@@ -1,9 +1,12 @@
 package com.friendbook.userservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.friendbook.userservice.model.User;
 import com.friendbook.userservice.model.UserBooksWantToRead;
 
 @Repository
@@ -11,4 +14,6 @@ public interface UserBooksWantToReadRepository extends JpaRepository<UserBooksWa
 
     @Query("SELECT count(ubw)>0 FROM UserBooksWantToRead ubw WHERE ubw.user.id=:userId AND ubw.bookId=:bookId")
     boolean getSavingByBookIdAndUserId(Long bookId, Long userId);
+
+    Optional<UserBooksWantToRead> getUserBooksWantToReadByBookIdAndAndUser(Long bookId, User user);
 }
